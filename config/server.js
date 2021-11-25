8bfa0e10e6b4ce851aaed5f476b6024762056e5f
Valentin Bousquet <bousquet.valentin@gmail.com>
2021-11-25 17:45:29 +0100

    npx create-strapi-app Strapi—Boilerplate --quickstart

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ec36c74ce163ead946871014b7c0f143'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '675afb6a-4aea-4b10-bc6e-dd9c4510b665'
+};
